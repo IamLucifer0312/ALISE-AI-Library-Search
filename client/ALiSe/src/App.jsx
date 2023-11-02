e75f15777527418406a0020d868fe79c0d763101
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2023-11-02 19:26:45 +0700

    front-end update

--- client/ALiSe/src/App.jsx
+++ client/ALiSe/src/App.jsx
@@ -6,31 +6,44 @@ import axios from "axios"
 
 function App() {
   const [inputValue, setInputValue] = useState(""); 
+  const [userPrompt, setUserPrompt] = useState(""); // State to store user's input
+  const [chat, setChat] = useState([]);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value); 
   }
 
-  const handleClick = async () => {
+  const handleClick = async () => {    
     const response = await axios.post("http://localhost:3000/prompt", {
       userPrompt: inputValue,
     });
-    console.log(response);
+    console.log(response.data);
+    setChat(response.data)
   }
 
   return (
-    <div class='container'>
-      <div class = 'header'>ALiSe</div>
-      <div class = 'chatbox'>
-        <div class = 'message' id = 'user-prompt'>Hello fasjkfjadsfjasfadsfadsfasdfadsfsdfdsaf</div>
-        <div class = 'message' id = 'AI-answer-title'>Here's what I have found</div>
-        <div class = 'message' id = 'AI-answers'>Library one</div>
-        <div id = 'chat-bar'>
+    <div className='container'>
+      <div className = 'header'>ALiSe</div>
+      <div className = 'chatbox'>
+         <div className = 'message' id = 'user-prompt'> </div>
+        {chat.map((entry, key) => {
+          if (entry.role == "system") return
+
+          if (entry.role == "user") {
+            return <div className = 'message user-message' key={key}>{entry.content}</div>  
+          }
+
+          if (entry.role == "assistant") {
+            return <div className = 'message ai-message' key={key}>{entry.content}</div>  
+          }
+
+        })}
+      </div>
+      <div id = 'chat-bar'>
           <input type="text" id="chat-input" placeholder="Type a message..." value={inputValue}
             onChange={handleInputChange}/>
           <button id ="chat-button" onClick={handleClick}>Send</button>
-        </div>
-      </div>
+       </div>
     </div>
   );
 }
