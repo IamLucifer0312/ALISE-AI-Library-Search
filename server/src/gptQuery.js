3227c02e5bc800d8131904075bd28cffdae6ae09
MasterMobo <mastermobo101@gmail.com>
2023-11-04 00:06:39 +0700

    Update gptQuery.js

--- server/src/gptQuery.js
+++ server/src/gptQuery.js
@@ -42,7 +42,7 @@ const getGPTResponse = async (role, prompt) => {
         const response =  await axios.post(
             "https://api.openai.com/v1/chat/completions",
             {
-                model: "gpt-3.5-turbo",
+                model: "gpt-4",
                 temperature: 0.6,
                 messages: chat,
             },
